71e9a92243228fe3950044c8e34b5a1c2e1df965
Kris Gray <kgray@justise.com>
2017-05-27 15:22:26 -0700

    New Jobs Page

--- jobs.php
+++ jobs.php
@@ -16,7 +16,7 @@
                 </div>
             </div>
             <div class="content-text">
-			    <div class="statement">
+                <div class="statement">
                      <h1>
                          Be paid to give love to the cutest cats and dogs in the world.
                      </h1>
@@ -28,55 +28,52 @@
                     <div class="yui3-u-2-3">
                         <div class="grid-wrapper">
                             <p>
-				                Pawblem Solved is hiring for a Part-Time Pet Nanny and Dog Walker. Even if you’ve never considered pet sitting in your
-                future before, take a look at all the benefits of this enjoyable and rewarding job.
-                                <ul>
-                                    <li>Starting rate of $11 per 30 minute walk/visit. (1 hour visits net you $22)</li>
-                                    <li>Typical work weeks range from 5-15 hours per week.</li>
-                                    <li>Reimbursement for gas.</li>
-                                    <li>Holiday Pay.</li>
-                                    <li>You set your schedule and decide which jobs to take on.</li>
-                                    <li>Bonuses based on team member’s commitment, enthusiasm and hard-work.</li>
-                                    <li>Working for a small company which values providing the very best care for pets and their
-                                    humans above all else.</li>
-                                    <li>Mentoring and training on the finer points of pet nanny services and dog walking.</li>
-                                    <li>Lots of excercise and fresh air in a fun, stress-free work enviornment, with a supportive, family-like team.</li>
-                                </ul>
-			                </p>
-		                    <h3>What We Are Looking For:</h3>
-                            <p>
-			                    <ul>
-                                    <li>Someone interested in part-time, mid-day (11am-3pm), weekly work, with occasional weekends, mornings & evenngs.</li>
-                                    <li>Someone living in the Bellevue, Redmond or Kirkland area. <i>At this time, we are hiring primarily to take on Redmond pets.</i></li>
-                                    <li>Clean criminal record.</li>
-                                    <li>A person who doesn’t mind a little rain with their day.</li>
-                                    <li>A trusty vehicle for getting around.</li>
-                                    <li>Lastly, you should label yourself as a true animal lover.</li>
-			                    </ul>
-                            </p>		 
-			                <h3>
-				                About Us:
-			                </h3>
-                            <br/>
-                            <p>
-                               Pawblem Solved is a small, reputable business, which looks to provide amazing service to the pets of 
-                               the Bellevue, Redmond and Kirkland areas. We offer 30 minute Monday through Friday leashed dog walks 
-                               for those pet owners that work long days and can't get home to their pups, as well as 30 minute 
-                               pet-sitting visits. We focus on healthy and happy pets that are absolutely impossible to resist. 
-                               To see our team, visit our <a href="http://www.pawblemsolved.com/petsitters.php">teams page</a>.
+                                We are always searching for new team members, who love animals and the great outdoors! 
                             </p>
-			
-			                <h3>
-				                To Apply, Please Send:
-			                </h3>
                             <p>
-			                    <ul>
-				                    <li>Resume</li>
-				                    <li>Contact information including; phone number and home address</li>
-				                    <li>Cover letter describing your experience(s) with animals and any other distinguished information
-                    that sets you apart from other candidates. The more pet experience you have, the better!</li>
-			                    </ul>
-			                </p>
+                                Here are just a few examples of how dog walking &amp; pet sitting could fit into your life - 
+                                <ul>
+                                    <li>You're retired and searching for something fun to do with your time, that will get you exercise and fresh air.</li>
+                                    <li>You have a part-time morning or evening job, but are in need of a little extra cash.</li>
+                                    <li>You're a mother, but have some availability while the kids are at school, and would love to be part of a small, family-like team.</li>
+                                </ul>
+                            </p>
+                            <p>
+                                Please keep in mind that we never overload our schedules, so it's important that the individual understands this is a part-time position.
+                            </p>
+                            <p>
+                                Our schedules vary each day, but generally occur - 
+                                <ul>
+                                    <li>Monday - Friday </li>
+                                    <li>11am - 2pm </li>
+                                    <li>With occasional kitty visits on the weekends</li>
+                                    <li>Typical work-weeks range from 5-15 hours; you set your schedule &amp; decide which jobs to take on</li>
+                                </ul>
+                            </p>         
+
+                            <p>
+                                Here are some benefits of the job - 
+                                <ul>
+                                    <li>You get to be around animals all day! Nothing is better than a purr of a cat, or the wag of a tail to greet you at the door.</li>
+                                    <li>Reimbursement for gas</li>
+                                    <li>Bonuses based on team member's commitment, enthusiasm, and hard work</li>
+                                    <li>Holiday pay</li>
+                                    <li>Monthly coffee dates to catch-up with the team</li>
+                                </ul>
+                            </p>
+                            <p>
+                                Compensation is approximately $150 - $300 per week
+                            </p>
+            
+                            
+                            <p>
+                                If you're interested in joining our team, please reply to the link below and include -
+                                <ul>
+                                    <li>Resume</li>
+                                    <li>Contact info; phone + home address </li>
+                                    <li>Cover letter describing your experiences with animals and any other distinguished info that sets you apart from other candidates. The more pet experience you have, the better! </li>
+                                </ul>
+                            </p>
                             <p class="center">
                                 <a class="button" target="_blank" href="mailto:info@pawblemsolved.com?subject=Applying for Pet Nanny Position">Apply Now</a>
                             </p>
@@ -93,8 +90,8 @@
 
               
             </div>
-			<br/>
-			<br/>
+            <br/>
+            <br/>
         </div>
         <?PHP include('includes/footer.php'); ?>
     </div>
